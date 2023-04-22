ae19e990ae9d1c4790ad5209871bc1d15734dcd1
JuanLicciardi <juanelicciardi@gmail.com>
2023-04-21 21:38:42 -0300

    Proyecto Operaciones Matematicas

---
+++ OperacionesMatematicas/src/Operaciones.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan Licciardi
+ */
+public class Operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+    }
+    
+    public static void suma(int a, int b){
+    
+    }
+    public static void resta(int a, int b){
+    
+    }
+    public static void multiplicacion(int a, int b){
+    
+    }
+    public static void division(int a, int b){
+    
+    }
+    
+}
